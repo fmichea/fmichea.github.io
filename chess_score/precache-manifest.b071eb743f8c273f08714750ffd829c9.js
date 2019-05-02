4b9b2df1a5ddb679bf54426ff0706b6e340af921
Franck Michea <franck.michea@gmail.com>
2019-05-02 21:31:16 +0200

    Add chess-score release to pages.

---
+++ chess_score/precache-manifest.b071eb743f8c273f08714750ffd829c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f01ec21daa2208964a64b8e125dd2e",
+    "url": "/chess_score/index.html"
+  },
+  {
+    "revision": "526c9f9bb7ac75d44f35",
+    "url": "/chess_score/static/css/2.5272414f.chunk.css"
+  },
+  {
+    "revision": "28061bd8eb5e56dcc04b",
+    "url": "/chess_score/static/css/main.15896e8d.chunk.css"
+  },
+  {
+    "revision": "526c9f9bb7ac75d44f35",
+    "url": "/chess_score/static/js/2.7611ddf2.chunk.js"
+  },
+  {
+    "revision": "28061bd8eb5e56dcc04b",
+    "url": "/chess_score/static/js/main.4a2e6eb1.chunk.js"
+  },
+  {
+    "revision": "9d65bcab6eb66174ca38",
+    "url": "/chess_score/static/js/runtime~main.52b43ae0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/chess_score/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/chess_score/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/chess_score/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/chess_score/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/chess_score/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
